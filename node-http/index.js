4e5b0153b530934ff2b1ea7b3c6746cb7f6095e3
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-03-28 19:23:47 +0100

    Add serving static html files

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,49 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const port = 3000;
 const hostname = 'localhost';
 
 
 const server = http.createServer((req, res)=>{
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello, Node!</h1></body></html>');
+    console.log(`Request for ${req.url} by method ${req.method}`);
+    if (req.method === 'GET') {
+        let fileUrl;
+        if (req.url === '/') {
+            fileUrl = '/index.html';
+        } else {
+            fileUrl = req.url;
+        }
+
+        let filePath = path.resolve(`./public${fileUrl}`);
+        const fileExt = path.extname(filePath);
+        if (fileExt === '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>${fileUrl} not found</h1></body></html>`)
+                    return
+                }
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>${fileUrl} not a HTML file</h1></body></html>`)
+            return
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>${req.method} not supported</h1></body></html>`)
+        return
+    }
+
 });
 
 server.listen(port, hostname, ()=> {

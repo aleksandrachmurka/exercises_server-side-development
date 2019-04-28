ee80d8ef17d67e70db338778db54e2ca2abbfed3
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-04-28 15:28:20 +0200

    Add cookie to authentication process

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -31,27 +31,38 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('abcdefg-1234567'));
 
 function auth(req, res, next) {
-  let authHeader = req.headers.authorization;
-  if (!authHeader) {
-    let err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-  let auth = new Buffer(authHeader.split(' ')[1], 'base64')
-  .toString().split(':');
-  let username = auth[0];
-  let password = auth[1];
-  if (username == 'admin' && password == 'password') {
-    next();
+  if (!req.signedCookies.username) {
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+      let err = new Error('You are not authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+    let auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
+    .toString().split(':');
+    let username = auth[0];
+    let password = auth[1];
+    if (username == 'admin' && password == 'password') {
+      res.cookie('username', 'admin', {signed: true});
+      next();
+    } else {
+      let err = new Error('You are not authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    let err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if (req.signedCookies.username === 'admin') {
+      next();
+    } else {
+      let err = new Error('You are not authenticated');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 

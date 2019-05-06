7d1825857db45db46ee5a47c63d952578eaa5847
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-05-06 21:11:58 +0200

    Add image files uploading

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/usersRouter');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
@@ -58,6 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

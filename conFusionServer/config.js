5e760e0e3a9b21dd25042b26d1923a653c7ffa63
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-05-08 22:09:28 +0200

    Add authentication using Passport-Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '123456789987654321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2327075114227239',
+        clientSecret: '711d8beca83b0246d3ab2d77d6294851'
+    }
 }
